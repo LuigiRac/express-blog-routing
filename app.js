4e71fa977baeca17a01cd27105e145672765b491
LuigiRac <lui.rac06@gmail.com>
2024-12-03 13:47:22 +0100

    first setting

---
+++ app.js
@@ -0,0 +1,23 @@
+/*
+Usando l'array dei post con le relative immagini, creare un file di routing (routers/posts.js) che conterrà le rotte necessarie per l'entità post.
+All'interno creare le rotte per le operazioni CRUD (Index, Show, Create, Update e Delete)
+Tutte le risposte saranno dei testi che confermeranno l’operazione che il server deve eseguire, secondo le convenzioni REST.
+*/
+
+const express = require('express');
+const port = 3000;
+const app = express();
+
+const ricetta = require('./data/posts.js');
+
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}}`);
+});
+
+// app.get("/", (req, res) => {
+//     res.send("Ciao");
+// });
+
+// app.get('/bacheca', (req, res) => {
+//     res.json(ricetta)
+// });
---
+++ data/posts.js
@@ -0,0 +1,39 @@
+const posts = [
+    {
+        id: 1,
+        titolo: "Ciambellone",
+        contenuto: "Ciambellone",
+        immagine: "/img/ciambellone.jpeg",
+        tags: ["Dolce", "Ricetta"]
+    },
+    {
+        id: 2,
+        titolo: "Cracker Barbabietola",
+        contenuto: "Cracker Barbabietola",
+        immagine: "/img/cracker_barbabietola.jpeg",
+        tags: ["Cracker", "Ricetta", "Barbabietola"]
+    },
+    {
+        id: 3,
+        titolo: "Pane Fritto Dolce",
+        contenuto: "Pane Fritto Dolce",
+        immagine: "/img/pane_fritto_dolce.jpeg",
+        tags: ["Pane", "Ricetta", "Dolce"]
+    },
+    {
+        id: 4,
+        titolo: "Pasta Barbabietola",
+        contenuto: "Pasta Barbabietola",
+        immagine: "/img/pasta_barbabietola.jpeg",
+        tags: ["Pasta", "Ricetta", "Barbabietola"]
+    },
+    {
+        id: 5,
+        titolo: "Torata Paesana",
+        contenuto: "Torta Paesana",
+        immagine: "/img/torta_paesana.jpeg",
+        tags: ["Torta", "Ricetta", "Dolce"]
+    }
+];
+
+module.exports = posts;
