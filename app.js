c8e0e565633d78ebd1e091bea25d613b6a3c3986
LuigiRac <lui.rac06@gmail.com>
2024-12-04 16:52:40 +0100

    read - show

--- app.js
+++ app.js
@@ -7,11 +7,28 @@ Tutte le risposte saranno dei testi che confermeranno l’operazione che il serv
 const express = require('express');
 const port = 3000;
 const app = express();
+app.use(express.static("public")); //http://localhost:3000/
+//const { postsRouter } = require('./routers/posts') // multiple export/import
+const postsRouter = require('./routers/posts')
+// console.log(postsRouter);
+//return;
 
-
-
-app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}}`);
+// url: /
+app.get('/', (req, res) => {
+    res.send('Home page')
 });
 
-use
+
+// Aggiungo Middleware con le rotte di /posts
+// /routers/posts - /routers/posts/3
+// /posts - /posts/3
+app.use('/posts', postsRouter);
+
+// url: tutte quelle che non corrispondono a niente di definito prima
+app.get('*', (req, res) => {
+    res.status(404).send('404 page')
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
--- routers/posts.js
+++ routers/posts.js
@@ -1,11 +1,66 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
 
+const posts = [
+    {
+        id: 1,
+        titolo: "Ciambellone",
+        contenuto: "Ciambellone",
+        immagine: "/img/ciambellone.jpeg",
+        tags: ["Dolce", "Ricetta"]
+    },
+    {
+        id: 2,
+        titolo: "Cracker Barbabietola",
+        contenuto: "Cracker Barbabietola",
+        immagine: "/img/cracker_barbabietola.jpeg",
+        tags: ["Cracker", "Ricetta", "Barbabietola"]
+    },
+    {
+        id: 3,
+        titolo: "Pane Fritto Dolce",
+        contenuto: "Pane Fritto Dolce",
+        immagine: "/img/pane_fritto_dolce.jpeg",
+        tags: ["Pane", "Ricetta", "Dolce"]
+    },
+    {
+        id: 4,
+        titolo: "Pasta Barbabietola",
+        contenuto: "Pasta Barbabietola",
+        immagine: "/img/pasta_barbabietola.jpeg",
+        tags: ["Pasta", "Ricetta", "Barbabietola"]
+    },
+    {
+        id: 5,
+        titolo: "Torata Paesana",
+        contenuto: "Torta Paesana",
+        immagine: "/img/torta_paesana.jpeg",
+        tags: ["Torta", "Ricetta", "Dolce"]
+    }
+];
 
 
-//READ - INDEX
-router.get('/post', (req, res) => {
-    res.send('Lista dei post')
+//READ - INDEX - /posts/
+router.get('/', (req, res) => {
+    res.json(posts); // restituisce JSON dei posts
+});
+
+// READ - SHOW: /posts/1, /posts/2 ...
+router.get('/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+    const post = posts.find((post) => {
+        return post.id == id;
+    });
+    if (post) {
+        res.send("Post con id: " + id + " Con titolo: " + post.titolo);
+    }
+
+    res.send("Post non trovato");
 });
 
 
+
+
+//const postsRouter = router;
+//module.exports = { postsRouter, posts } // multiple export
+module.exports = router;
